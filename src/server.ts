455de6c20dd8781fc82f6f8597facb2cc23e94cf
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-06 13:26:50 +0000

    created routes for comments requests

--- src/server.ts
+++ src/server.ts
@@ -43,12 +43,49 @@ app.post("/pastes", async (req, res) => {
   }
 });
 
-app.delete("/pastes/:id", async (req, res) => {
+app.delete("/pastes/:pasteId", async (req, res) => {
   try {
     const query = "DELETE FROM pastes WHERE id=$1";
-    const id = req.params.id;
+    const id = req.params.pasteId;
     await client.query(query, [id]);
     res.status(200).send("Item deleted");
+  } catch (error) {
+    res.send(error);
+    console.error(error);
+  }
+});
+
+app.get("/pastes/:pasteId/comments", async (req, res) => {
+  try {
+    const query = "SELECT * FROM comments WHERE paste_id = $1";
+    const id = req.params.pasteId;
+    const response = await client.query(query, [id]);
+    res.status(200).send(response.rows);
+  } catch (error) {
+    res.send(error);
+    console.error(error);
+  }
+});
+
+app.post("/pastes/:pasteId/comments", async (req, res) => {
+  try {
+    const query =
+      "INSERT INTO comments (paste_id, comment, username) VALUES ($1, $2, $3)";
+    const values = [req.params.pasteId, req.body.comment, req.body.username];
+    await client.query(query, values);
+    res.status(200).send("comment posted");
+  } catch (error) {
+    res.send(error);
+    console.error(error);
+  }
+});
+
+app.delete("/pastes/comments/:commentId", async (req, res) => {
+  try {
+    const query = "DELETE FROM comments WHERE comment_id = $1";
+    const commentId = req.params.commentId;
+    await client.query(query, [commentId]);
+    res.status(200).send("comment deleted");
   } catch (error) {
     res.send(error);
     console.error(error);
